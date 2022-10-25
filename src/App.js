102dff1553a586864d7623243788937256062360
thahkem <hauhauhau22446688@gmail.com>
2022-10-25 02:30:13 -0700

    Programs component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.scss';
 import Hero from './components/Hero/Hero';
+import Programs from './components/Programs/Programs';
 function App() {
-  return (
-    <div className="App">
-          <Hero />
-    </div>
-  );
+    return (
+        <div className="App">
+            <Hero />
+            <Programs />
+        </div>
+    );
 }
 
 export default App;
